21cc8a864f37acfbcb42ea0caba080b89d0ffd0d
Alex-Moreror <97977300+Alex-Moreror@users.noreply.github.com>
2022-01-18 22:29:33 +0300

    Add files via upload

---
+++ Программист(а,ов).py
@@ -0,0 +1,43 @@
+a = str("программист")
+b = str("программиста")
+c = str("программистов")
+d = int(input())
+g = d % 10 # выдаст остаток вследствие чего я смогу понять какое окончание
+if d == 11:
+    print(d, c)
+elif d == 12:
+    print(d, c)
+elif d == 13:
+    print(d, c)
+elif d == 14:
+    print(d, c)
+elif d == 111:
+    print(d, c)
+elif d == 112:
+    print(d, c)
+elif d == 113:
+    print(d, c)
+elif d == 114:
+    print(d, c)
+elif g == 1:
+    print(d, a)
+elif g == 2:
+    print(d, b)
+elif g == 3:
+    print(d, b)
+elif g == 4:
+    print(d, b)
+elif g == 5:
+    print(d, c)
+elif g == 6:
+    print(d, c)
+elif g == 7:
+    print(d, c)
+elif g == 8:
+    print(d, c)
+elif g == 9:
+    print(d, c)
+elif g == 0:
+    print(d, c)
+
+ # ПРОГРАММИСТ 1 ПРОГРАММИСТА 2,3,4 ПРОГРАММИСТОВ 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,0
